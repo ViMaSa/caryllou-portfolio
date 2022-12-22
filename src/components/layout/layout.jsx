caf221a4664644ac81f512bd80dfa63dac868500
Victor Sarmiento <sarmiv07@gmail.com>
2022-12-22 12:31:17 -0800

    Menu bars completed

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -3,11 +3,21 @@ import { Link, Outlet } from 'react-router-dom';
 import ReactModal from 'react-modal';
 import { useState } from 'react';
 
+ReactModal.setAppElement('#root')
+
 const Layout = () => {
   const [ modalIsOpen, setIsOpen ] = useState(false);
 
   const openModal = () => {
     setIsOpen(true);
+  }
+
+  const onAfterOpen = () => {
+    document.body.style.overflow = 'hidden';
+  }
+
+  const onAfterClose = () => {
+    document.body.style.overflow = 'unset';
   }
 
   const closeModal = () => {
@@ -20,29 +30,33 @@ const Layout = () => {
         <div className='homeButton'>
           <Link to='/' ><p>clp.</p></Link>
         </div>
-        <div>
-          <button onClick={openModal}>Open Modal</button>
-          <ReactModal
-            isOpen={modalIsOpen}
-            onRequestClose={closeModal}
-            className='Modal'
-            overlayClassName='Overlay'
-            contentLabel='Example Modal'
-          >
-            <ul>
-              <li onClick={closeModal}>
-                <Link to='/portfolio' className='modal-font'>Portfolio</Link>
-              </li>
-              <li onClick={closeModal}>
-                <Link to='/resume' className='modal-font'>Resume</Link>
-              </li>
-              <li onClick={closeModal}>
-                <Link to='/about' className='modal-font'>About</Link>
-              </li>
-              <p onClick={closeModal} className='modal-font'>X</p>
-            </ul>
-          </ReactModal>
+        <div className='menu-bars-container' onClick={openModal}>
+          <div className='menu-bar' />
+          <div className='menu-bar' />
+          <div className='menu-bar' />
         </div>
+        <ReactModal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          onAfterOpen={onAfterOpen}
+          onAfterClose={onAfterClose}
+          className='Modal'
+          overlayClassName='Overlay'
+          contentLabel='Example Modal'
+        >
+          <ul>
+            <li onClick={closeModal}>
+              <Link to='/portfolio' className='modal-font'>Portfolio</Link>
+            </li>
+            <li onClick={closeModal}>
+              <Link to='/resume' className='modal-font'>Resume</Link>
+            </li>
+            <li onClick={closeModal}>
+              <Link to='/about' className='modal-font'>About</Link>
+            </li>
+            <p onClick={closeModal} className='modal-font'>X</p>
+          </ul>
+        </ReactModal>
       </nav>
       <Outlet/>
     </>
