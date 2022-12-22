1ced727e96c09b63068e47e8f150b9729a4443fa
Victor Sarmiento <sarmiv07@gmail.com>
2022-12-21 21:54:40 -0800

    Modal added. TODO: Menu bars & footer links need to scroll up

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,24 +1,48 @@
-import { Link, Outlet } from "react-router-dom";
-import './layout.scss'
+import './layout.scss';
+import { Link, Outlet } from 'react-router-dom';
+import ReactModal from 'react-modal';
+import { useState } from 'react';
 
 const Layout = () => {
+  const [ modalIsOpen, setIsOpen ] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  }
+
+  const closeModal = () => {
+    setIsOpen(false);
+  }
+
   return (
     <>
       <nav>
-        <div className="homeButton">
+        <div className='homeButton'>
           <Link to='/' ><p>clp.</p></Link>
         </div>
-        <ul>
-          <li>
-            <Link to='/portfolio'>Portfolio</Link>
-          </li>
-          <li>
-            <Link to='/resume'>Resume</Link>
-          </li>
-          <li>
-            <Link to='/about'>About</Link>
-          </li>
-        </ul>
+        <div>
+          <button onClick={openModal}>Open Modal</button>
+          <ReactModal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            className='Modal'
+            overlayClassName='Overlay'
+            contentLabel='Example Modal'
+          >
+            <ul>
+              <li onClick={closeModal}>
+                <Link to='/portfolio' className='modal-font'>Portfolio</Link>
+              </li>
+              <li onClick={closeModal}>
+                <Link to='/resume' className='modal-font'>Resume</Link>
+              </li>
+              <li onClick={closeModal}>
+                <Link to='/about' className='modal-font'>About</Link>
+              </li>
+              <p onClick={closeModal} className='modal-font'>X</p>
+            </ul>
+          </ReactModal>
+        </div>
       </nav>
       <Outlet/>
     </>
